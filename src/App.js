4090794125e210c1e36ffefd2308e92f5ccfb008
Anonymous2024-spec <shifahnana2@gmail.com>
2025-03-21 18:33:50 +0300

    React Router concept. Added Hoem, About us page for practice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
-import ComponentA from "./ComponentA";
-// import Counter from "./CounterWithEffect";
-// import UserData from "./UserData";
-
-// const people = [
-//   {
-//     id: 0,
-//     name: "Creola Katherine Johnson",
-//     profession: "mathematician",
-//   },
-//   {
-//     id: 1,
-//     name: "Mario José Molina-Pasquel Henríquez",
-//     profession: "chemist",
-//   },
-//   {
-//     id: 2,
-//     name: "Mohammad Abdus Salam",
-//     profession: "physicist",
-//   },
-//   {
-//     id: 3,
-//     name: "Percy Lavon Julian",
-//     profession: "chemist",
-//   },
-//   {
-//     id: 4,
-//     name: "Subrahmanyan Chandrasekhar",
-//     profession: "astrophysicist",
-//   },
-// ];
+import HomePage from "./HomePage";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
-    <div>
-      {/* <Counter /> */}
-      {/* <UserData /> */}
-      <ComponentA/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
