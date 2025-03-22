92ba4640f70d90ecec5608679b960a4a6f62bb5d
Anonymous2024-spec <shifahnana2@gmail.com>
2025-03-22 21:28:04 +0300

    Code about the Store:- Zustand concept

--- src/App.js
+++ src/App.js
@@ -15,3 +15,6 @@ function App() {
 }
 
 export default App;
+
+// create an application that has a public route and protected route
+// u can use an object and use it to check on whether u logggedin or not, so that ony loggedin users are the only one who can access protected routes
